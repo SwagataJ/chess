ddeaf81b6b04930ad298b18b777395477a2aa9b2
Swagata Jana <65404204+SwagataJ@users.noreply.github.com>
2021-12-09 19:57:38 +0530

    Initial commit, added necessary files

---
+++ static/scripts.js
@@ -0,0 +1,197 @@
+let board,
+    game = new Chess(),
+    statusEl = $('#status'),
+    fenEl = $('#fen'),
+    pgnEl = $('#pgn');
+
+
+// do not pick up pieces if the game is over
+// only pick up pieces for the side to move
+let onDragStart = function (source, piece, position, orientation) {
+    if (game.game_over() === true ||
+        (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+        return false;
+    }
+};
+
+let onDrop = function (source, target) {
+    // see if the move is legal
+    let move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    });
+
+    // illegal move
+    if (move === null) return 'snapback';
+
+    updateStatus();
+    getResponseMove();
+};
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+let onSnapEnd = function() {
+    board.position(game.fen());
+};
+
+let updateStatus = function() {
+  let status;
+
+  let moveColor = 'White';
+  if (game.turn() === 'b') {
+    moveColor = 'Black';
+  }
+
+  // checkmate?
+  if (game.in_checkmate() === true) {
+    status = 'Game over, ' + moveColor + ' is in checkmate.';
+  }
+
+  // draw?
+  else if (game.in_draw() === true) {
+    status = 'Game over, drawn position';
+  }
+
+  // game still on
+  else {
+    status = moveColor + ' to move';
+
+    // check?
+    if (game.in_check() === true) {
+      status += ', ' + moveColor + ' is in check';
+    }
+  }
+
+  setStatus(status);
+  getLastCapture();
+  createTable();
+  updateScroll();
+
+  statusEl.html(status);
+  fenEl.html(game.fen());
+  pgnEl.html(game.pgn());
+};
+
+let cfg = {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+};
+
+let randomResponse = function() {
+    fen = game.fen()
+    $.get($SCRIPT_ROOT + "/move/" + fen, function(data) {
+        game.move(data, {sloppy: true});
+        // board.position(game.fen());
+        updateStatus();
+    })
+}
+
+let getResponseMove = function() {
+    let e = document.getElementById("sel1");
+    let depth = e.options[e.selectedIndex].value;
+    fen = game.fen()
+    $.get($SCRIPT_ROOT + "/move/" + depth + "/" + fen, function(data) {
+        game.move(data, {sloppy: true});
+        updateStatus();
+        // This is terrible and I should feel bad. Find some way to fix this properly.
+        // The animations would stutter when moves were returned too quick, so I added a 100ms delay before the animation
+        setTimeout(function(){ board.position(game.fen()); }, 100);
+    })
+}
+
+
+// did this based on a stackoverflow answer
+// http://stackoverflow.com/questions/29493624/cant-display-board-whereas-the-id-is-same-when-i-use-chessboard-js
+setTimeout(function() {
+    board = ChessBoard('board', cfg);
+    // updateStatus();
+}, 0);
+
+
+let setPGN = function() {
+  let table = document.getElementById("pgn");
+  let pgn = game.pgn().split(" ");
+  let move = pgn[pgn.length - 1];
+}
+
+let createTable = function() {
+
+    let pgn = game.pgn().split(" ");
+    let data = [];
+
+    for (let i = 0; i < pgn.length; i += 3) {
+        let index = i / 3;
+        data[index] = {};
+        for (let j = 0; j < 3; j++) {
+            let label = "";
+            if (j === 0) {
+                label = "moveNumber";
+            } else if (j === 1) {
+                label = "whiteMove";
+            } else if (j === 2) {
+                label = "blackMove";
+            }
+            if (pgn.length > i + j) {
+                data[index][label] = pgn[i + j];
+            } else {
+                data[index][label] = "";
+            }
+        }
+    }
+
+    $('#pgn tr').not(':first').remove();
+    let html = '';
+    for (let i = 0; i < data.length; i++) {
+        html += '<tr><td>' + data[i].moveNumber + '</td><td>'
+        + data[i].whiteMove + '</td><td>'
+        + data[i].blackMove + '</td></tr>';
+    }
+
+    $('#pgn tr').first().after(html);
+}
+
+let updateScroll = function() {
+    $('#moveTable').scrollTop($('#moveTable')[0].scrollHeight);
+}
+
+let setStatus = function(status) {
+  document.getElementById("status").innerHTML = status;
+}
+
+let takeBack = function() {
+    game.undo();
+    if (game.turn() !== "w") {
+        game.undo();
+    }
+    board.position(game.fen());
+    updateStatus();
+}
+
+let newGame = function() {
+    game.reset();
+    board.start();
+    updateStatus();
+}
+
+let getCapturedPieces = function() {
+    let history = game.history({ verbose: true });
+    for (let i = 0; i < history.length; i++) {
+        if ("captured" in history[i]) {
+            console.log(history[i]["captured"]);
+        }
+    }
+}
+
+let getLastCapture = function() {
+    let history = game.history({ verbose: true });
+    let index = history.length - 1;
+
+    if (history[index] !== undefined && "captured" in history[index]) {
+        console.log(history[index]["captured"]);
+    }
+}
